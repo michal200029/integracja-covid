4c866d19d0790f69bc31c28437be597c7bae7e4a
Michał Opałka <michalopalka2@gmail.com>
2022-05-25 16:11:36 +0200

    add feign dependencies

---
+++ src/main/java/tm/integracja/Projekt/integracja/config/FeignConfig.java
@@ -0,0 +1,11 @@
+package tm.integracja.Projekt.integracja.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public ApiCl
+}
---
+++ src/main/java/tm/integracja/Projekt/integracja/currencyscoop/CurrencyScoopClient.java
@@ -0,0 +1,2 @@
+package tm.integracja.Projekt.integracja.currencyscoop;public class CurrencyScoopClient {
+}
