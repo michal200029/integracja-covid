52f7435ac51d1794a2e721a84c2230e3888f0677
Michał Opałka <michalopalka2@gmail.com>
2022-05-25 18:05:51 +0200

    Delete @Postmapping

--- src/main/java/tm/integracja/Projekt/integracja/currencyscoop/CurrencyScoopHandler.java
+++ src/main/java/tm/integracja/Projekt/integracja/currencyscoop/CurrencyScoopHandler.java
@@ -1,20 +1,17 @@
 package tm.integracja.Projekt.integracja.currencyscoop;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
 import tm.integracja.Projekt.integracja.currencyscoop.model.HistoricalCurrencyValuesResponse;
 
 import java.time.LocalDate;
 
-
-@RestController
+@Component
 @AllArgsConstructor
 public class CurrencyScoopHandler {
 
     private final CurrencyScoopClient currencyScoopClient;
 
-    @PostMapping("/test")
     public HistoricalCurrencyValuesResponse getHistoricalValues(String base, String symbol, LocalDate localDate) {
         String apiKey = "e7030b0681415d2697a7f7b751476df0";
         String date = String.format("%s-%s-%s",
